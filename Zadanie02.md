a07b9d102569b0ad152e3481ca38cb8a77327fa6
Gerwix <mateuszgerwazik@gmail.com>
2021-12-09 18:31:14 +0100

    Initial commit

---
+++ Zadanie02.md
@@ -0,0 +1,4 @@
+Zadanie 2
+---
+
+Napisz skrypt który przekształca temperaturę podaną w stopniach Celsjusza na temperaturę w stopniach Fahrenheita.
---
+++ zadanie2.js
@@ -0,0 +1,3 @@
+var f = +prompt('Podaj temperaturę Fahrenheita: ');
+var value1 = (f - 32) * 5 / 9;
+alert('Podana temperatura Fahrenheita to: ' + value1 + "º" + ' C');
